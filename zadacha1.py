d4c403c0a516063f8a68642d9e6e3802bfeb3f2e
Gulnur <gulnur69@bk.ru>
2022-12-19 15:56:33 +0500

    zadacha1

---
+++ zadacha1.py
@@ -0,0 +1,21 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+from unittest import result
+
+
+with open('incoming_text_021.txt','w', encoding='UTF-8' ) as file:
+    file.write(input('Введите первоночальный текст: '))
+with open('incoming_text_021.txt','r') as file:
+    my_text = file.readline()
+    change_text = my_text.split()
+print()
+print (my_text)
+print()
+
+del_text = input('Введите набор букв, который нужно удалить из слов содержащие данную последовательность : ')
+print (del_text)
+
+' ' = ' '.join(filter(lambda x: del_text not in x, change_text))
+with open('format_text_021.txt','w', encoding='UTF-8') as file:
+    file.write(f'{result}')
+print (result)
